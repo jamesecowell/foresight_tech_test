ceaaafd3772c2b489b2af62fe065367696bc3cce
James Cowell <james.cowell28@gmail.com>
2020-06-10 15:43:53 +0100

    added Events and Exceptions to delegate

--- lib/blocs/simple_delegate.dart
+++ lib/blocs/simple_delegate.dart
@@ -2,8 +2,20 @@ import 'package:bloc/bloc.dart';
 
 class MySimpleBlocDelegate extends BlocDelegate {
   @override
+  void onEvent(Bloc bloc, Object event) {
+    print(event);
+    super.onEvent(bloc, event);
+  }
+
+  @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     print('bloc: $bloc, transition: $transition');
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print('$error, $stackTrace');
   }
 }
